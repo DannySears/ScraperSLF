06688e633c08e28f71ca7fdc909c0687397b55e3
Danny Sears <danny@sears6.com>
2023-12-02 22:02:40 -0600

    no more scripting, now navigating to the database page

--- scraper.py
+++ scraper.py
@@ -3,33 +3,49 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import datetime
 
-driver_path = os.path.join(os.path.dirname(__file__), 'drivers', 'chromedriver')
+def main():
+    driver_path = os.path.join(os.path.dirname(__file__), 'drivers', 'chromedriver')
 
-driver = webdriver.Chrome()
-driver.get('https://cijspub.co.collin.tx.us/PublicAccess/CaseDetail.aspx?CaseID=2336564')
+    #boot driver to home page
+    driver = webdriver.Chrome()
+    driver.get('https://cijspub.co.collin.tx.us/PublicAccess/CaseDetail.aspx?CaseID=2336564')
+
+    #get the button for case records and click
+    time.sleep(1)
+    caseRecordsLink = driver.find_element(By.XPATH, "//div[@id='divOption1']/a[contains(@class, 'ssSearchHyperlink')]")
+    caseRecordsLink.click()
+
+    time.sleep(1)
+
+    #click date filed button
+    dateFiledButton = driver.find_element(By.ID, "DateFiled")
+    dateFiledButton.click()
+    time.sleep(1)
+
+    #enter the current date to the date filed fields
+    today = datetime.date.today()
+
+    today = today - datetime.timedelta(days=1)
+    d1 = today.strftime("%m/%d/%Y")
+
+    datefiledAfterField = driver.find_element(By.ID, "DateFiledOnAfter")
+    datefiledAfterField.send_keys(d1)
+    datefiledBeforeField = driver.find_element(By.ID, "DateFiledOnBefore")
+    datefiledBeforeField.send_keys(d1)
+
+    #find and click search button
+    searchButton = driver.find_element(By.ID, "SearchSubmit")
+    searchButton.click()
 
 
-tables = driver.find_elements(By.TAG_NAME, 'table')
 
-defendant_name = driver.find_element(By.XPATH, "//table")
-print(defendant_name)
-# Check if there are at least three tables
-if len(tables) >= 3:
-    # Select the third table
-    third_table = tables[4]
+    time.sleep(10)
+    driver.quit()
 
-    # Find the first 'tr' element in the third table
-    # If you need a specific 'tr', you can adjust this part
-    tr_element = third_table.find_elements(By.TAG_NAME,'tr')
-
-    rows = third_table.find_elements(By.TAG_NAME, 'tr')
-    
-    for row in rows:
-    # Find all 'td' elements in the row
-        cells = row.find_elements(By.TAG_NAME, 'td')
-    for cell in cells:
-        print(cell.text)
-
-# Don't forget to close the driver
-driver.quit()
+if __name__ == '__main__':
+    main()
